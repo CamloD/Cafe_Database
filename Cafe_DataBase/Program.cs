e9d9c303b4532b952e82bfbb666ae47047405959
Sergio Salcedo <sergado_310@outlook.com>
2024-01-27 01:25:26 -0500

    cambios pequeños

--- Cafe_DataBase/Program.cs
+++ Cafe_DataBase/Program.cs
@@ -18,7 +18,6 @@ namespace Cafe_DataBase
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MenuPrincipal());
-
         }
     }
 }
